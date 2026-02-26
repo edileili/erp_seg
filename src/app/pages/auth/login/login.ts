daaaaca36c6758543fc6d00899abd93170a5d11a
edileili <edileila65@gmail.com>
2026-02-25 20:38:54 -0600

    Más correcciones para Practica 3

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
-import { FormsModule } from '@angular/forms';
-import { SelectModule } from 'primeng/select';
-import { InputGroupModule } from 'primeng/inputgroup';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-login',
-  imports: [CardModule, InputGroupModule, InputNumberModule, InputTextModule, FormsModule, SelectModule, ButtonModule],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, InputGroupModule, CardModule, ToastModule],
+  providers: [MessageService],
+  templateUrl: './login.html'
 })
-export class Login {
+export class Login implements OnInit {
+  private messageService = inject(MessageService);
+  loginForm!: FormGroup;
 
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+onSubmit() {
+  if (this.loginForm.valid) {
+    const { email, password } = this.loginForm.value;
+
+    if (email === 'eden@gmail.com' && password === 'Contrasenia$') {
+      this.messageService.add({
+        severity: 'success',
+        summary: '¡Éxito!',
+        detail: 'Bienvenido a ERP',
+        life: 3000 
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error de acceso',
+        detail: 'Usuario o contraseña incorrectos',
+      });
+    }
+  }
+}
 }
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputNumberModule } from 'primeng/inputnumber';
@@ -18,7 +18,7 @@ import { DatePickerModule } from 'primeng/datepicker';
     CardModule, ButtonModule, ToastModule, ReactiveFormsModule, PasswordModule, DatePickerModule,
     InputGroupModule, InputNumberModule, InputTextModule, InputGroupAddonModule,
   ],
-  providers: [MessageService], 
+  providers: [MessageService],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -29,54 +29,52 @@ export class Register {
   public registroSeg: FormGroup;
   public formSubmitted = false;
 
-constructor() {
-  this.registroSeg = this.fb.group({
-    usuario: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    contrasenia: ['', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/)]],
-    concontrasenia: ['', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/)]],
-    nomCompleto: ['', Validators.required],
-    direccion: ['', Validators.required],
-    // Cambiamos el control de edad por fecha con el validador personalizado
-    fechaNacimiento: [null, [Validators.required, this.validarMayoriaEdad.bind(this)]], 
-    telefono: [null, [Validators.required, Validators.pattern("^[0-9]{10,}$")]]
-  });
-}
-  validarMayoriaEdad(control: any) {
-  const fechaSeleccionada = control.value;
-  if (!fechaSeleccionada) return null;
-
-  const hoy = new Date();
-  const cumpleanos = new Date(fechaSeleccionada);
-  let edad = hoy.getFullYear() - cumpleanos.getFullYear();
-  const mes = hoy.getMonth() - cumpleanos.getMonth();
-
-  // Ajuste si aún no ha cumplido años en el año actual
-  if (mes < 0 || (mes === 0 && hoy.getDate() < cumpleanos.getDate())) {
-    edad--;
+  constructor() {
+    this.registroSeg = this.fb.group({
+      usuario: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      contrasenia: ['', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/)]],
+      concontrasenia: ['', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/)]],
+      nomCompleto: ['', Validators.required],
+      direccion: ['', Validators.required],
+      fechaNacimiento: [null, [Validators.required, this.validarMayoriaEdad.bind(this)]],
+      telefono: [null, [Validators.required, Validators.pattern("^[0-9]{10,}$")]]
+    });
   }
+  validarMayoriaEdad(control: any) {
+    const fechaSeleccionada = control.value;
+    if (!fechaSeleccionada) return null;
 
-  return edad >= 18 ? null : { menorDeEdad: true };
-}
+    const hoy = new Date();
+    const cumpleanos = new Date(fechaSeleccionada);
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    const mes = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < cumpleanos.getDate())) {
+      edad--;
+    }
+
+    return edad >= 18 ? null : { menorDeEdad: true };
+  }
   onSubmit() {
     this.formSubmitted = true;
-    
+
     if (this.registroSeg.valid) {
-      this.messageService.add({ 
-        severity: 'success', 
-        summary: 'Éxito', 
-        detail: 'Registro completado', 
-        life: 3000 
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Éxito',
+        detail: 'Registro completado',
+        life: 3000
       });
       console.log(this.registroSeg.value);
       this.registroSeg.reset();
       this.formSubmitted = false;
     } else {
-      this.messageService.add({ 
-        severity: 'error', 
-        summary: 'Error', 
-        detail: 'Por favor, revisa los campos', 
-        life: 3000 
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Por favor, revisa los campos',
+        life: 3000
       });
     }
   }
