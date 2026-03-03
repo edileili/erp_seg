ed7b7793a7bb849cd3d2488143cd1c9f10a4e7d0
edileili <edileila65@gmail.com>
2026-03-03 15:15:18 -0600

    Practica 6. Añadir un grupo y editar datos del usuario

--- src/app/pages/groups/groups.ts
+++ src/app/pages/groups/groups.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { BadgeModule } from 'primeng/badge';
 import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 
 @Component({
   selector: 'app-groups',
-  imports: [CardModule, BadgeModule, OverlayBadgeModule],
+  imports: [CardModule, BadgeModule, OverlayBadgeModule, DialogModule, ButtonModule, InputGroupAddonModule, InputGroupModule],
   templateUrl: './groups.html',
   styleUrl: './groups.css',
 })
 export class Groups {
+  visible: boolean = false;
 
+    showDialog() {
+        this.visible = true;
+    }
 }
--- src/app/pages/user/user.ts
+++ src/app/pages/user/user.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
-
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { Password } from 'primeng/password';
+import { DatePicker } from 'primeng/datepicker';
+import { InputNumber } from 'primeng/inputnumber';
 @Component({
   selector: 'app-user',
-  imports: [CardModule],
+  imports: [CardModule, ButtonModule, DialogModule, InputGroupModule, InputGroupAddonModule, Password, DatePicker, InputNumber],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
+  visible: boolean = false;
 
+    showDialog() {
+        this.visible = true;
+    }
 }
